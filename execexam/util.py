42547df5b2c074840ec6a88bdedcddc1360c5add
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2024-08-15 12:09:36 -0400

    chore: Delete the execexam/util.py file as it is not strictly needed.

--- execexam/util.py
+++
@@ -1,7 +0,0 @@
-"""Supporting assertions for test suites using execexam."""
-
-
-def assert_and_print(expected, actual, message):
-    """Print a diagnostic message and assert that the expected and actual values are equal."""
-    print(f"{message}: Expected {expected}, got {actual}")  # noqa: T201
-    assert expected == actual
