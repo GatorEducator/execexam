60f8283e624db45bb3502c70a2984b7d9466d817
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2024-08-17 21:26:29 -0400

    feat: Add the starting version of the execexam/extract.py file.

---
+++ execexam/extract.py
@@ -0,0 +1,24 @@
+"""Extract contents from data structures."""
+
+from typing import Any, Dict
+
+
+def extract_details(details: Dict[Any, Any]) -> str:
+    """Extract the details of a dictionary and return it as a string."""
+    output = []
+    # iterate through the dictionary and add each key-value pair
+    for key, value in details.items():
+        output.append(f"{value} {key}")
+    if len(output) == 0:
+        return ""
+    return "Details: " + ", ".join(output)
+
+
+def extract_test_run_details(details: Dict[Any, Any]) -> str:
+    """Extract the details of a test run."""
+    # Format of the data in the dictionary:
+    # 'summary': Counter({'passed': 2, 'total': 2, 'collected': 2})
+    summary_details = details["summary"]
+    # convert the dictionary of summary to a string
+    summary_details_str = extract_details(summary_details)
+    return summary_details_str
