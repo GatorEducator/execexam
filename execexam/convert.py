d1273ef12e5c46b1d5d3d97114d41d2c432fecc6
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2024-08-17 20:29:46 -0400

    feat: Add starting version of the execexam/convert.py file.

---
+++ execexam/convert.py
@@ -0,0 +1,17 @@
+"""Perform data conversions."""
+
+from pathlib import Path
+
+
+def path_to_string(path_name: Path, levels: int = 4) -> str:
+    """Convert the path to an elided version of the path as a string."""
+    # break the path into parts
+    parts = path_name.parts
+    # note that this uses the - 1 because
+    # the first part is always the root
+    # (for instance, on Unix-like systems, '/'
+    # and on Windows, 'C:\')
+    if len(parts) - 1 > levels:
+        return Path("<...>", *parts[-levels:]).as_posix()
+    else:
+        return path_name.as_posix()
