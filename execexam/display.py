34644a9af75d6d80bfd29778dae676d48a77e097
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2024-08-21 21:12:57 -0400

    feat: Add two supporting functions for output in execexam/display.py.

--- execexam/display.py
+++ execexam/display.py
@@ -1,9 +1,44 @@
 """Display results from running the execexam tool."""
 
+from typing import Any, Dict
+
 from rich.console import Console
+from rich.markdown import Markdown
 from rich.panel import Panel
 from rich.syntax import Syntax
 from rich.text import Text
+
+
+def make_colon_separated_string(arguments: Dict[str, Any]):
+    """Make a colon separated string from a dictionary."""
+    return "\n" + "".join(
+        [f"- {key}: {value}\n" for key, value in arguments.items()]
+    )
+
+
+def display_diagnostics(  # noqa: PLR0913
+    verbose: bool,
+    console: Console,
+    content: str,
+    label: str,
+    richtext: bool,
+    syntax: bool,
+    syntax_theme: str = "ansi_dark",
+    syntax_language: str = "python",
+) -> None:
+    """Display a diagnostic message using rich or plain text."""
+    if verbose:
+        display_content(
+            console,
+            content,
+            label,
+            richtext,
+            syntax,
+            syntax_theme,
+            syntax_language,
+        )
+    else:
+        return None
 
 
 def display_content(  # noqa: PLR0913
