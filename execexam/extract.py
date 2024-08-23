52186dee1e7e5be11f1fb12f99e456dc93c0eeff
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2024-08-23 15:55:23 -0400

    refactor: Add all of the extract functions to the execexam/extract.py file.

--- execexam/extract.py
+++ execexam/extract.py
@@ -1,6 +1,9 @@
 """Extract contents from data structures."""
 
-from typing import Any, Dict
+from pathlib import Path
+from typing import Any, Dict, List, Tuple
+
+from . import convert
 
 
 def extract_details(details: Dict[Any, Any]) -> str:
@@ -22,3 +25,120 @@ def extract_test_run_details(details: Dict[Any, Any]) -> str:
     # convert the dictionary of summary to a string
     summary_details_str = extract_details(summary_details)
     return summary_details_str
+
+
+def extract_test_assertion_details(test_details: Dict[Any, Any]) -> str:
+    """Extract the details of a dictionary and return it as a string."""
+    # create an empty list to store the output
+    output = []
+    # indicate that this is the first assertion
+    # to be processed (it will have a "-" to start)
+    first = True
+    # iterate through the dictionary and add each key-value pair
+    # that contains the details about the assertion
+    for key, value in test_details.items():
+        # this is the first assertion and thus
+        # the output will start with a "-"
+        if first:
+            output = ["  - "]
+            output.append(f"{key}: {value}\n")
+            first = False
+        # this is not the first assertion and thus
+        # the output will start with a "  " to indent
+        else:
+            output.append(f"    {key}: {value}\n")
+    # return each index in the output list as a string
+    return "".join(output)
+
+
+def extract_test_assertion_details_list(details: List[Dict[Any, Any]]) -> str:
+    """Extract the details of a list of dictionaries and return it as a string."""
+    output = []
+    # iterate through the list of dictionaries and add each dictionary
+    # to the running string that conatins test assertion details
+    for current_dict in details:
+        output.append(extract_test_assertion_details(current_dict))
+    return "".join(output)
+
+
+def extract_test_assertions_details(test_reports: List[dict[str, Any]]):
+    """Extract the details of test assertions."""
+    # create an empty list that will store details about
+    # each test case that was execued and each of
+    # the assertions that was run for that test case
+    test_report_string = ""
+    # iterate through the list of test reports
+    # where each report is a dictionary that includes
+    # the name of the test and the assertions that it ran
+    for test_report in test_reports:
+        # get the name of the test
+        test_name = test_report["nodeid"]
+        # extract only the name of the test file and the test name,
+        # basically all of the content after the final slash
+        display_test_name = test_name.rsplit("/", 1)[-1]
+        test_report_string += f"\n{display_test_name}\n"
+        # there is data about the assertions for this
+        # test and thus it should be extracted and reported
+        if "assertions" in test_report:
+            test_report_string += extract_test_assertion_details_list(
+                test_report["assertions"]
+            )
+    # return the string that contains all of the test assertion details
+    return test_report_string
+
+
+def extract_failing_test_details(
+    details: dict[Any, Any],
+) -> Tuple[str, List[Dict[str, Path]]]:
+    """Extract the details of a failing test."""
+    # extract the tests from the details
+    tests = details["tests"]
+    # create an empty string that starts with a newline;
+    # the goal of the for loop is to incrementally build
+    # of a string that contains all deteails about failing tests
+    failing_details_str = "\n"
+    # create an initial path for the file containing the failing test
+    failing_test_paths = []
+    # incrementally build up results for all of the failing tests
+    for test in tests:
+        if test["outcome"] == "failed":
+            current_test_failing_dict = {}
+            # convert the dictionary of failing details to a string
+            # and add it to the failing_details_str
+            failing_details = test
+            # get the nodeid of the failing test
+            failing_test_nodeid = failing_details["nodeid"]
+            failing_details_str += f"  Name: {failing_test_nodeid}\n"
+            # get the call information of the failing test
+            failing_test_call = failing_details["call"]
+            # get the crash information of the failing test's call
+            failing_test_crash = failing_test_call["crash"]
+            # extract the root of the report, which corresponds
+            # to the filesystem on which the tests were run
+            failing_test_path_root = details["root"]
+            # extract the name of the file that contains the test
+            # from the name of the individual test case itself
+            failing_test_nodeid_split = failing_test_nodeid.split("::")
+            # create a complete path to the file that contains the failing test file
+            failing_test_path = (
+                Path(failing_test_path_root) / failing_test_nodeid_split[0]
+            )
+            # extract the name of the function from the nodeid
+            failing_test_name = failing_test_nodeid_split[-1]
+            # assign the details about the failing test to the dictionary
+            current_test_failing_dict["test_name"] = failing_test_name
+            current_test_failing_dict["test_path"] = failing_test_path
+            failing_test_paths.append(current_test_failing_dict)
+            # creation additional diagnotics about the failing test
+            # for further display in the console in a text-based fashion
+            failing_test_path_str = convert.path_to_string(
+                failing_test_path, 4
+            )
+            failing_test_lineno = failing_test_crash["lineno"]
+            failing_test_message = failing_test_crash["message"]
+            # assemble all of the failing test details into the string
+            failing_details_str += f"  Path: {failing_test_path_str}\n"
+            failing_details_str += f"  Line number: {failing_test_lineno}\n"
+            failing_details_str += f"  Message: {failing_test_message}\n"
+    # return the string that contains all of the failing test details
+    return (failing_details_str, failing_test_paths)
