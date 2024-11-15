17e8bf7fe7700bc3ee76bb85280d5bfa2f10b8ba
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2024-11-15 09:47:45 -0500

    coms: Add comments to tests/test_main.py.

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,6 +6,14 @@ from execexam import main
 
 runner = CliRunner()
 
+# NOTE: tests that run execexam through the its CLI
+# using the CliRunner can run into dependency issues
+# due to the fact that the pytest plugin that
+# execexam uses is going to be repeatedly loaded
+# and (potentially) not unloaded
+
+# Tests that provide valid arguments {{{
+
 
 def test_run_use_help():
     """Test the run command with the --help."""
@@ -24,6 +32,12 @@ def test_run_use_tldr():
     assert "--help" in result.output
 
 
+# }}}
+
+
+# Tests that provide invalid arguments {{{
+
+
 def test_run_valid_argument_no_action():
     """Test the run command with valid required arguments."""
     result = runner.invoke(main.cli, ["run", ". tests/"])
@@ -34,3 +48,6 @@ def test_run_invalid_report_argument():
     """Test the run command with invalid report argument."""
     result = runner.invoke(main.cli, ["run", ". tests/", "--report invalid"])
     assert result.exit_code != 0
+
+
+# }}}
